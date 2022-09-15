6f00d5c2efb39807bb78f6257f67819ab707103d
Matheus Barboza <matheus.barboza102@gmail.com>
2022-09-15 16:28:56 -0300

    criando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import "./styles/main.css";
 
+import * as Dialog from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
 import logoImg from "./assets/logo-nlw-esports.svg";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 import { GameBanner } from "./components/GameBanner";
 
+interface Game {
+  id: string;
+  bannerUrl: string;
+  title: string;
+  _count: {
+    ads: number;
+  };
+}
+
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  }, []);
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -18,23 +39,83 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={5}
-        />
-        <GameBanner bannerUrl="/game-2.png" title="Dota 2" adsCount={2} />
-        <GameBanner bannerUrl="/game-3.png" title="CS:GO" adsCount={5} />
-        <GameBanner bannerUrl="/game-4.png" title="Apex Legends" adsCount={5} />
-        <GameBanner bannerUrl="/game-5.png" title="Fortnite" adsCount={5} />
-        <GameBanner
-          bannerUrl="/game-6.png"
-          title="World of Warcraft"
-          adsCount={5}
-        />
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+            <Dialog.Title className="text-3xl text-white font-black">
+              Publique um anúncio
+            </Dialog.Title>
+
+            <Dialog.Content>
+              <form>
+                <div>
+                  <label htmlFor="game">Qual o game?</label>
+                  <input
+                    id="game"
+                    placeholder="Selecione o game que deseja jogar"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="name">Seu nome (ou nickname)</label>
+                  <input
+                    id="name"
+                    placeholder="Como te chamam dentro do game?"
+                  />
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                    <input
+                      id="yearsPlaying"
+                      type="number"
+                      placeholder="Tudo bem ser ZERO"
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="discord">Qual seu Discord?</label>
+                    <input
+                      id="discord"
+                      type="text"
+                      placeholder="Usuário#0000"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="weekDays">Quando costuma jogar?</label>
+                  </div>
+                  <div>
+                    <label htmlFor="hourStart">Qual horário do dia?</label>
+                    <div>
+                      <input id="hourStart" type="time" placeholder="De" />
+                      <input id="hourEnd" type="time" placeholder="Até" />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
